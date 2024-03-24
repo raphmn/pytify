d5ab6b66b35834b83ee9101f4935b59c06ef0d5c
raphmn <rmalleron@icloud.com>
2024-03-24 19:00:04 +0100

    Reorganized folders

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import tkinter as tk
 from PIL import Image
 import ctypes
 
-from player_frame import *
+from app.player_frame import *
+from app.navigator import *
 
 applicationID = 'nsi.project.spotify.1_0'  # arbitrary string
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(applicationID)
@@ -20,43 +21,11 @@ class App(ctk.CTk):
         self.title('Spotify')
         self.iconbitmap('media/appicon/appicon.ico')
 
-
-        self.lateralBar = ctk.CTkFrame(self, width=180, height=500)
-        self.lateralBar.pack_propagate(0)
-        self.lateralBar.pack(anchor=tk.NW, side="left")
-
-        self.spotify_label = ctk.CTkLabel(self.lateralBar, text='Spotify')
-        self.spotify_label.pack(expand=True)
-
-        self.albums_image = ctk.CTkImage(Image.open('media/icons/albums_32.png'))
-        self.albums_button = ctk.CTkButton(self.lateralBar, text='Albums', command=self.show_albums,
-                                           image=self.albums_image)
-        self.albums_button.pack(ipadx=0, ipady=20, expand=True)
-
-        self.artists_image = ctk.CTkImage(Image.open('media/icons/artists_32.png'))
-        self.artists_button = ctk.CTkButton(self.lateralBar, text='Artists', command=self.show_artists,
-                                            image=self.artists_image)
-        self.artists_button.pack(ipadx=0, ipady=20, expand=True)
-
-        self.search_image = ctk.CTkImage(Image.open('media/icons/search_32.png'))
-        self.search_button = ctk.CTkButton(self.lateralBar, text='Search', command=self.search_button,image=self.search_image)
-        self.search_button.pack(ipadx=0, ipady=20, expand=True)
+        self.navigator = LateralFrame(self, width=180, height=500)
+        self.navigator.pack(anchor=tk.NW, side="left")
 
         self.player_bar = PlayerFrame(self, width=820, height=50)
         self.player_bar.pack(anchor="s", side="bottom")
-
-
-    def search_button(self):
-        print('Search')
-        #### A completer
-
-    def show_artists(self):
-        print('Artists')
-        ### A completer
-
-    def show_albums(self):
-        print('Albums')
-        #### A completer
 
 
 app = App()
